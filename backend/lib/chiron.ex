9919449f51ddd5d58eb2cfd5cf97c18c81444495
Gabriel Jaldon <gjaldon85@gmail.com>
2014-10-23 01:31:44 +0800

    Set up :chiron_registry ETS

--- backend/lib/chiron.ex
+++ backend/lib/chiron.ex
@@ -6,6 +6,9 @@ defmodule Chiron do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    setup_ets_table
+    store_couch_host_url
+
     children = [
       # Define workers and child supervisors to be supervised
       # worker(Chiron.Worker, [arg1, arg2, arg3])
@@ -15,5 +18,19 @@ defmodule Chiron do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Chiron.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def host_url do
+    [{"couch_host", url}] = :ets.lookup(:chiron_registry, "couch_host")
+    url
+  end
+
+  defp setup_ets_table do
+    :ets.new(:chiron_registry, [:named_table, :set, :protected])
+  end
+
+  defp store_couch_host_url do
+    host = System.get_env("COUCH_DB_HOST") || "http://127.0.0.1:5984"
+    :ets.insert(:chiron_registry, {"couch_host", host})
   end
 end
